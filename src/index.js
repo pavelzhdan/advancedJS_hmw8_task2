bedfc0f878cb439f7fe7360732c03357647e147e
Pavel Zhdanovich <zhdan21@gmail.com>
2020-05-14 21:22:46 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+export default class ErrorRepository {
+  constructor() {
+    this.errorsArray = new Map();
+  }
+
+  translate(code) {
+    if (this.errorsArray.has(code)) {
+      return this.errorsArray.get(code);
+    }
+    return Error('Unknown error');
+  }
+}
+
+const errorsDataBase = new ErrorRepository();
+
+errorsDataBase.errorsArray.set(404, 'страница не найдена');
+errorsDataBase.errorsArray.set(400, 'рандомная ошибка');
---
+++ src/js/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import ErrorRepository from '../../index';
+
+test('error found', () => {
+  const errorsDataBase = new ErrorRepository();
+  errorsDataBase.errorsArray.set(404, 'страница не найдена');
+  errorsDataBase.errorsArray.set(400, 'рандомная ошибка');
+
+  expect(errorsDataBase.translate(404)).toBe('страница не найдена');
+});
+
+test('error not found', () => {
+  const errorsDataBase = new ErrorRepository();
+  errorsDataBase.errorsArray.set(404, 'страница не найдена');
+  errorsDataBase.errorsArray.set(400, 'рандомная ошибка');
+
+  expect(errorsDataBase.translate(401)).toEqual(Error('Unknown error'));
+});
